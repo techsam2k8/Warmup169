32e21d2ce276b47fb2fff4da9183631b7517cfb9
Samuel Ahn <techsam2k8@gmail.com>
2013-02-11 23:50:10 -0800

    finished for now

--- app/models/users.rb
+++ app/models/users.rb
@@ -1,37 +1,37 @@
 class Users < ActiveRecord::Base
   attr_accessible :count, :password, :user
   
-  validates :user, :uniqueness => true, :presence => true, :length => { :maximum => 128 }
-  validates :password, :length => { :maximum => 128 }
+  # validates :user, :uniqueness => true, :presence => true, :length => { :maximum => 128 }
+  # validates :password, :length => { :maximum => 128 }
   
-  def self.login(user, password)
-    result = self.find(:first, :conditions => { :user => user, :password => password})
-    if result == nil
-        return -1
-    end
-    count = result.count + 1
-    result.update_attribute(:count, count)
-    return count
-  end
+  # def self.login(user, password)
+    # result = self.find(:first, :conditions => { :user => user, :password => password})
+    # if result == nil
+        # return -1
+    # end
+    # count = result.count + 1
+    # result.update_attribute(:count, count)
+    # return count
+  # end
   
-  def self.add(user, password)
-    result = self.find(:first, :conditions => { :user => user })
-    if result != nil
-        return -2
-    end
-    if user == "" || user.length > 128
-        return -3
-    end
-    if password.length > 128
-        return -4
-    end
-    account = self.create(:user => user, :password => password, :count => 1)
-    return account.count
+  # def self.add(user, password)
+    # result = self.find(:first, :conditions => { :user => user })
+    # if result != nil
+        # return -2
+    # end
+    # if user == "" || user.length > 128
+        # return -3
+    # end
+    # if password.length > 128
+        # return -4
+    # end
+    # account = self.create(:user => user, :password => password, :count => 1)
+    # return account.count
     
-  end
+  # end
   
-  def self.TESTAPI_resetFixture()
-    self.delete_all
-    return 1
-  end
+  # def self.TESTAPI_resetFixture()
+    # self.delete_all
+    # return 1
+  # end
 end
--- test/unit/users_test.rb
+++ test/unit/users_test.rb
@@ -21,5 +21,43 @@ class UsersTest < ActiveSupport::TestCase
     assert_respond_to(controller, 'unittests')
   end
   
+  test "CreateUser" do
+    user = Users.create(:user => 'user', :password => 'password', :count => 1)
+    assert_not_nil(Users.find(:first, :conditions => {:user => 'user'}))
+  end
   
+  test "ResetTable" do
+    user = Users.create(:user => 'user', :password => 'password', :count => 1)
+    Users.delete_all
+    assert_nil(Users.find(:first, :conditions => {:user => 'user'}))
+  end
+  
+  test "CheckPassword" do
+    user = Users.create(:user => 'user', :password => 'password', :count => 1)
+    assert_equal('password', Users.find(:first, :conditions => {:user => 'user'}).password)
+  end
+  
+  test "CheckCount" do
+    user = Users.create(:user => 'user', :password => 'password', :count => 1)
+    assert_equal(1, Users.find(:first, :conditions => {:user => 'user'}).count)
+  end
+
+  test "UpdateCount" do
+    user = Users.create(:user => 'user', :password => 'password', :count => 1)
+    count = user.count + 1
+    user.update_attribute(:count, count)
+    assert_equal(2, Users.find(:first, :conditions => {:user => 'user'}).count)
+  end
+  
+  test "UserColumnExists" do
+    assert(Users.column_names.include?('user'))
+  end
+
+  test "PasswordColumnExists" do
+    assert(Users.column_names.include?('password'))
+  end
+  
+  test "CountColumnExists" do
+    assert(Users.column_names.include?('count'))
+  end
 end
